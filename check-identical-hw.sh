3685869a66f6235381a6caa80835a2cbd9f7d5a9
ng0cduy <buiduyngoc1@gmail.com>
2023-10-09 23:10:25 +0700

    update files to 20231009231025

--- check-identical-hw.sh
+++ check-identical-hw.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 HW_file="hw.txt"
-HW_layered_folder="$HOME/Desktop/newHW_layered"
+OS=$(uname -m)
+if [ "$OS" = "x86_64" ]
+then
+    HW_layered_folder="$Desktop/newHW_layered"
+elif [ "$OS" = "arm64" ]
+then
+    HW_layered_folder="$HOME/Desktop/newHW_layered"
+fi
 ls "$HW_layered_folder" > "temp_list.txt"
-echo "*************************"
 while read line;
     do
         if grep -q "$line" "temp_list.txt";
