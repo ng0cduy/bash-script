b38bd72d46ac56ef1fd2227de170484e17ea5603
ng0cduy <buiduyngoc1@gmail.com>
2024-02-12 00:10:07 +0700

    update files to 20240212001007

--- cleanupWorkspace.sh
+++ cleanupWorkspace.sh
@@ -2,19 +2,28 @@
 
 echo "Reading data from a file:"
 while read line; do
-  file_path=$(grep -r "$line" "phase/PHASE-IMG" | cut -d ":" -f1)
+  line1=$(echo "$line" | rev | cut -d "/" -f1 | rev)
+  file_path=$(grep -r "$line1" "phase/PHASE-IMG" | cut -d ":" -f1)
   file_path1=$(echo $file_path | cut -d '/' -f1)
   file_path2=$(echo $file_path | cut -d '/' -f2)
   file_path3=$(echo $file_path | cut -d '/' -f3)
   file_path4=$(echo $file_path | cut -d '/' -f4)
-  echo "$file_path is with ID: $line"
-  deleted_path="$file_path1/$file_path2/$file_path3/$file_path4"
-  echo "$deleted_path"
-  echo "deleting $deleted_path"
-  if [[ $deleted_path == "///" ]]
+  if [[ -z $file_path ]]
   then
-    echo "Error"
+    echo "Folder containes ID $line1 not exist"
   else
+    deleted_path="$file_path1/$file_path2/$file_path3/$file_path4"
+    echo "$file_path is with ID: $line1"
+    echo "$deleted_path"
+    echo "deleting $deleted_path"
     rm -rf "$deleted_path"
   fi
+
+
+#   if [[ $deleted_path == "///" ]]
+#   then
+#     echo "Error"
+#   else
+
+#   fi
 done < "del_list.txt"
