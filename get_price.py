dee3d5d1424a73435af53f5e65f1054ba94fdb57
ng0cduy <buiduyngoc1@gmail.com>
2024-04-12 09:57:35 +0700

    upgrade

--- get_price.py
+++ get_price.py
@@ -2,16 +2,48 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from deep_translator import GoogleTranslator
+from selenium.webdriver.chrome.options import Options
+import platform
+import sys
+def check_architecture():
+    arch = platform.machine()
+    if 'arm' in arch.lower():
+        return "ARM"
+    elif 'amd' in arch.lower() or "x86_64" in arch.lower():
+        return "AMD"
+    else:
+        return "Unknown"
+
+def check_system():
+    system = platform.system()
+    if system == 'Windows':
+        return "Windows"
+    elif system == 'Linux':
+        return "Linux"
+    else:
+        return "Unknown"
+
+print("Operating System:", check_system())
 
 # Path to your Chrome WebDriver executable
-webdriver_path = 'chromedriver-mac-arm64/chromedriver'
+if check_architecture() == "ARM":
+    webdriver_path = 'chromedriver-mac-arm64/chromedriver'
+else:
+    webdriver_path = 'chromedriver-linux64/chromedriver'
+    # chrome_binary='/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe'
+
+## Setup chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless") # Ensure GUI is off
+chrome_options.add_argument("--no-sandbox")
+chrome_options.binary_location = f"chrome-linux64/chrome"
 
 # URL of the webpage you want to scrape
 url = 'https://jp.mercari.com/en/item/m51252320310'
 
 # Initialize Chrome WebDriver
 service = Service(webdriver_path)
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service,options=chrome_options)
 driver.implicitly_wait(10)
 # Navigate to the webpage
 driver.get(url)
