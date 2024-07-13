068bd8ed07aabb302b432d4ac3131f1fb12ad692
Ngọc Duy Bùi <ngocduybui@Ngocs-MacBook-Pro.local>
2024-07-13 09:15:09 +0700

    update files to 20240713091509

--- curl_multiple_img.sh
+++ curl_multiple_img.sh
@@ -7,8 +7,13 @@ set -e
 # fi
 input_folder=$1
 input_file="curl_multiple_img.txt"
+number=0
+increment=1
+mkdir "$input_folder"
 # Read the file line by line
 while IFS= read -r url; do
+    filename="$number.jpg"
     # Use curl to fetch the content
-    curl -O "$input_folder/$url"
+    curl --output "$input_folder/$filename" "$url"
+    number=$((number + increment))
 done < "$input_file"
